78239e0c41280201f41184af2e4a309534560ca0
Rahul Yedida <yrahul3910@gmail.com>
2026-02-28 14:00:36 -0500

    feat: get changed file list

--- cmd/review.go
+++ cmd/review.go
@@ -2,6 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,7 +13,25 @@ var reviewCmd = &cobra.Command{
 	Use:   "review",
 	Short: "Review the current code changes",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("review called")
+		gitCmd := exec.Command(
+			"git",
+			"--no-pager",
+			"diff",
+			"--name-only",
+			"--cached",
+			"--merge-base",
+			"origin/main",
+		)
+		stdout, err := gitCmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		changedFiles := string(stdout)
+
+		for file := range strings.SplitSeq(changedFiles, "\n") {
+			fmt.Print(file)
+		}
 	},
 }
 
